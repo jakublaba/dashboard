dc6869da3a4b0d66aa87df51e69186f10f6dc177
Jakub Łaba <77542257+jakublaba@users.noreply.github.com>
2023-01-10 21:00:11 +0100

    Initial version of data model (#1)
    * Add zod dependency
    
    * Initial data model

---
+++ src/model/Order.ts
@@ -0,0 +1,31 @@
+import {z} from "zod";
+import {ProductSchema} from "./Product"
+import {UserSchema} from "./User";
+import {OrderStatus} from "./Status";
+
+export const OrderSchema = z.object({
+    id: z.bigint(),
+    seller: UserSchema
+        .refine((usr) => usr.role === "Seller", {message: "seller must have \"Seller\" role"}),
+    buyer: UserSchema
+        .refine((usr) => usr.role === "Buyer", {message: "buyer must have \"Buyer\" role"}),
+    products: z.map(ProductSchema, z.number())
+        .refine((map) => {
+            map.forEach((amount) => {
+                if (!Number.isInteger(amount)) {
+                    return false;
+                }
+            });
+        }, {message: "Amount of product must be an integer"})
+        .refine((map) => {
+            map.forEach((amount) => {
+                if (amount < 1) {
+                    return false;
+                }
+            })
+        }, {message: "At least 1 product must be ordered"}),
+    status: OrderStatus,
+    date: z.date()
+});
+
+export type Order = z.infer<typeof OrderSchema>;
---
+++ src/model/Product.ts
@@ -0,0 +1,8 @@
+import {z} from "zod";
+
+export const ProductSchema = z.object({
+    name: z.string().min(1).max(30),
+    price: z.number().min(1).max(Number.MAX_VALUE)
+});
+
+export type Product = z.infer<typeof ProductSchema>;
