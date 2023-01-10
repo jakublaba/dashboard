8e2d8e0f1c705836e3220e2d1842aed3bce64158
Jakub Łaba <77542257+jakublaba@users.noreply.github.com>
2023-01-10 22:22:02 +0100

    Remove some boilerplate junk

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import {createBrowserRouter, createRoutesFromElements, Route, RouterProvider} from "react-router-dom";
+import {ProductRanking} from "./components/ProductRanking";
+import {ctxAuth} from "./auth/auth";
+import {Orders} from "./components/Orders";
+import {CustomerFeedback} from "./components/CustomerFeedback";
+import {SalesChart} from "./components/SalesChart";
+import {MyAccount} from "./components/MyAccount";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    let [authState, setAuthState] = useState<object>({
+        isLoggedIn: false,
+        who: null
+    });
+
+    const login = (username: string): void => {
+        setAuthState({
+            isLoggedIn: true,
+            who: username
+        });
+    };
+
+    const logout = (): void => {
+        setAuthState({
+            isLoggedIn: false,
+            who: null
+        });
+    };
+
+    const router = createBrowserRouter(
+        createRoutesFromElements([
+            <Route path="/">
+                <Route path="orders" element={<Orders/>}></Route>
+                <Route path="feedback" element={<CustomerFeedback/>}></Route>
+                <Route path="ranking" element={<ProductRanking/>}></Route>
+                <Route path="chart" element={<SalesChart/>}></Route>
+                <Route path="account" element={<MyAccount/>}></Route>
+            </Route>
+        ])
+    );
+
+    return (
+        <div className="App">
+            <ctxAuth.Provider value={{authState, login, logout}}>
+                <RouterProvider router={router}/>
+            </ctxAuth.Provider>
+        </div>
+    );
 }
 
 export default App;
