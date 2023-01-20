792faaab05fca80e88a13d2feb6df6bc32ba7ebe
Jakub Łaba <77542257+jakublaba@users.noreply.github.com>
2023-01-20 23:33:35 +0100

    Update dependencies and organize stuff

--- src/auth/auth.ts
+++ src/auth/auth.ts
@@ -1,12 +1,10 @@
-import {Context, createContext} from "react";
-
-export const ctxAuth: Context<any> = createContext({} as Context<any>);
+import { useDispatch } from "react-redux";
+import IAction from "../types/auth-action.type";
 
 const users: Map<string, string> = new Map([
     ["user", "password"],
-    ["admin", "admin"]
+    ["admin", "admin"],
 ]);
 
-export const authenticate = (username: string, password: string): boolean => {
-    return users.get(username) === password;
-};
+export const authenticate = (username: string, password: string) =>
+    users.get(username) === password;
