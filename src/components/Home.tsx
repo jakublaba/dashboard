556312122ae24041db4233b8adb54f501ccfea10
Jakub Łaba <77542257+jakublaba@users.noreply.github.com>
2023-01-26 05:19:47 +0100

    Change app layout

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,20 +1,22 @@
 import React from "react";
 import {Box, Stack} from "@mui/material";
-import SalesChart from "./SalesChart";
+import SalesChart from "./widgets/chart/SalesChart";
 import ProductList from "./widgets/products/ProductList";
-import CommentList from "./widgets/feedback/CommentList";
+import CommentList from "./widgets/comments/CommentList";
 
 const Home: React.FC = () => {
     return (
         <Box>
-            <Stack
-                spacing={1}
-                direction={"row"}
-            >
-                <ProductList/>
-                <SalesChart/>
+            <Stack spacing={2}>
+                <Stack
+                    spacing={1}
+                    direction={"row"}
+                >
+                    <ProductList/>
+                    <SalesChart/>
+                </Stack>
+                <CommentList/>
             </Stack>
-            <CommentList/>
         </Box>
     );
 };
